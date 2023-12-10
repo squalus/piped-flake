0350ede97d6c54126ec1f109fc3d9c787f097bc6
squalus <squalus@squalus.net>
2023-12-10 12:28:07 -0800

    switch to nixpkgs 23.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "Random packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     systems.url = "github:nix-systems/default-linux";
     flake-utils = {
       url = "flake:flake-utils";
