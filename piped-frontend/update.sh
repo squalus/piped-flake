c9b8e6f30feef55daf926a916fd176c17cf796a8
squalus <squalus@squalus.net>
2023-12-27 09:34:36 -0800

    piped-frontend: remove prefetch-npm-deps bug workaround

--- piped-frontend/update.sh
+++ piped-frontend/update.sh
@@ -10,7 +10,6 @@ set -x
 ln -s "$srcDir"/package.json package.json
 npm install --package-lock-only --legacy-peer-deps --ignore-scripts
 cp package-lock.json "$pkgRoot"
-# prefetch-npm-deps fails intermittently when NIX_BUILD_CORES is not set to 1
-NIX_BUILD_CORES=1 prefetch-npm-deps package-lock.json | tr -d '\n' | jq -R -s '.'> "$pkgRoot"/npmDepsHash.json
+prefetch-npm-deps package-lock.json | tr -d '\n' | jq -R -s '.'> "$pkgRoot"/npmDepsHash.json
 cat "$pkgRoot"/npmDepsHash.json
 
